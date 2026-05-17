3729416b393dd775b076799b67d8fc0aca0ed2a0
Lucas Siqueira <lucashenriquesiqueira7@gmail.com>
2026-05-17 16:29:17 -0300

    Pacote security implementado

--- src/main/java/digital_catalog_api/demo/models/entities/UserAdmin.java
+++ src/main/java/digital_catalog_api/demo/models/entities/UserAdmin.java
@@ -6,7 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -16,12 +21,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class UserAdmin {
+public class UserAdmin implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String username;
     private String email;
     private String password;
     private Role role;
@@ -36,5 +40,40 @@ public class UserAdmin {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
--- src/main/java/digital_catalog_api/demo/repositories/UserAdminRepository.java
+++ src/main/java/digital_catalog_api/demo/repositories/UserAdminRepository.java
@@ -2,10 +2,12 @@ package digital_catalog_api.demo.repositories;
 
 import digital_catalog_api.demo.models.entities.UserAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserAdminRepository extends JpaRepository<UserAdmin, UUID> {
+    UserDetails findByEmail(String email);
 }
