0b02607205a64c2e2f3e52cc10306046d723a55e
Lucas Siqueira <lucashenriquesiqueira7@gmail.com>
2026-05-17 20:10:37 -0300

    Cors configurado

--- src/main/java/digital_catalog_api/demo/DemoApplication.java
+++ src/main/java/digital_catalog_api/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package digital_catalog_api.demo;
 import digital_catalog_api.demo.models.entities.UserAdmin;
 import digital_catalog_api.demo.models.entities.enums.Role;
 import digital_catalog_api.demo.repositories.UserAdminRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +17,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Value("${admin.email}")
+	private String adminEmail;
+
+	@Value("${admin.password}")
+	private String adminPassword;
+
 	@Bean
 	public CommandLineRunner seedAdmin(UserAdminRepository repository, PasswordEncoder encoder) {
 		return args -> {
-			if (repository.findByEmail("admin@loja.com").isEmpty()) {
+			if (repository.findByEmail(adminEmail).isEmpty()) {
 				UserAdmin admin = new UserAdmin();
-				admin.setEmail("admin@loja.com");
-				admin.setPassword(encoder.encode("senha123"));
+				admin.setEmail(adminEmail);
+				admin.setPassword(encoder.encode(adminPassword));
 				admin.setRole(Role.ADMIN);
 				repository.save(admin);
 				System.out.println("Admin criado com sucesso!");
